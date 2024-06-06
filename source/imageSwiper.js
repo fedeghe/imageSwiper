7a4a63501062cad5c0e94bee50a4a5ed18750c0d
fedeghe <fedeghe@gmail.com>
2024-06-06 22:18:02 +0200

    up

--- source/imageSwiper.js
+++ source/imageSwiper.js
@@ -1,6 +1,10 @@
 (function (W) {
     var WD = W.document;
-
+    function setX(el, property, els){
+        Object.entries(els).forEach(function (s){
+            el[property][s[0]] = s[1];
+        })
+    }
     function appendImageCb(el, trg, cb) {
         var imgcnt = document.createElement('div'),
             img0 = new Image(),
@@ -13,21 +17,21 @@
         function setImg(img, src, zIndex) {
             img.onload = ready;
             img.src = src;
-            img.style.position = 'absolute';
-            img.style.top = 0;
-            img.style.left = 0;
-            img.style.zIndex = zIndex;
+            setX(img, 'style', {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                zIndex: zIndex
+            });
             img.setAttribute('draggable', 'false');
             imgcnt.appendChild(img);
         }
         setImg(img0, el.image0src, 0);
         setImg(img1, el.image1src, 0);
-
         trg.style.position = 'relative';
         trg.appendChild(imgcnt);
         return img1;
     }
-
     function ImageSwiper(opts) {
         var self = this;
         this.opts = opts || {imageSwipe: true, rangeSwipe: true};
@@ -36,12 +40,10 @@
         this.separator = null;
         W.addEventListener('load', self.getItems.bind(self));
     }
-
     ImageSwiper.prototype.getItems = function () {
         var self = this,
             containers = WD.querySelectorAll('[data-imageswipe]'),
             howMany = containers.length;
-
         containers.forEach(function (container) {
             var images = container.dataset.imageswipe.split(';'),
                 el = {
@@ -52,7 +54,6 @@
                     image0src: images[0],
                     image1src: images[1]
                 };
-
             el.mainImg = appendImageCb(el, el.node, function () {
                 el.node.style.visibility = 'hidden';
                 el.node.style.height = (el.mainImg.height + (self.opts.rangeSwipe ? 30 : 0)) + 'px';
@@ -75,14 +76,14 @@
         this.els.forEach(function (el) {
             el.node.style.visibility = 'visible';
             el.separator = document.createElement('div');
-            el.separator.style.pointerEvents = 'none';
-            el.separator.style.position = 'absolute';
-            el.separator.style.width = 0;
-            el.separator.style.border = '0.5px dashed rgba(0, 0, 0, .2)';
-
-            el.separator.style.height = el.size.height + 'px';
-            el.separator.style.top = '0px';
-
+            setX(el.separator, 'style', {
+                pointerEvents : 'none',
+                position : 'absolute',
+                width : 0,
+                border : '0.5px dashed rgba(0, 0, 0, .2)',
+                height : el.size.height + 'px',
+                top : '0px'
+            });
             function updateClip(value) {
                 el.separator.style.opacity = 1;
                 el.mainImg.style.clip = 'rect(0, ' + value + 'px, ' + el.size.height + 'px, 0)';
@@ -91,42 +92,42 @@
                 if (value < 2 || value > el.size.width -2) {
                     el.separator.style.opacity = 0
                 }
-
             }
             function offsetUpdate(e, sliding) {
                 var oX = Math.abs(e.offsetX)
                 sliding && updateClip(oX)
             }
+
             function enableSliding() {
                 el.container.style.cursor = 'ew-resize';
                 el.sliding = true;
             }
-
             function disableSliding() {
                 el.container.style.cursor = 'default';
                 el.sliding = false;
             }
-
             if (self.opts.rangeSwipe) {
                 el.range = document.createElement('input');
                 el.range.type = 'range';
-                el.range.style.padding = 0;
-                el.range.style.margin = 0;
-                el.range.style.marginTop = '5px';
-                el.range.style.position = 'absolute';
-                el.range.style.width = el.size.width + 'px';
-                el.range.step = 1;
-                el.range.min = 0;
-                el.range.max = el.size.width;
-                el.range.value = 0;
+                setX(el.range, 'style', {
+                    padding: 0,
+                    margin: 0,
+                    marginTop: '5px',
+                    position: 'absolute',
+                    width: el.size.width + 'px'
+                });
+                setX(el, 'range', {
+                    step: 1,
+                    min: 0,
+                    max: el.size.width,
+                    value: 0
+                });
                 el.range.style.top = el.size.height + 'px';
-
                 el.node.appendChild(el.range)
                 el.range.addEventListener('input', function () {
                     updateClip(this.value)
                 });
             }
-
             if (self.opts.imageSwipe) {
                 el.sliding = false;
                 el.container.addEventListener('mousedown', enableSliding);
@@ -140,7 +141,6 @@
                 });
             }
             el.node.appendChild(el.separator)
-            
         });
     };
     W.ImageSwiper = function (opts) {
